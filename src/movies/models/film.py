88c6d3f81dcfbb57c7770647b2c5c330eda58457
Maxim Lashkov <maxim.lashkov@yandex.ru>
2024-12-09 21:24:56 +0200

    Добавлена бизнес-модель для фильмов

--- src/movies/models/film.py
+++ src/movies/models/film.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
+from typing import Optional, List
 from pydantic import BaseModel
 
 
 class Film(BaseModel):
     id: str
     title: str
-    description: str
+    description: Optional[str]
+    imdb_rating: float
+    directors: Optional[List]
+    actors: Optional[List]
+    writers: Optional[List]
