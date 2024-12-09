041224a7d08b5a7f7cacc9c04434e620fc154d40
Maxim Lashkov <maxim.lashkov@yandex.ru>
2024-12-10 00:08:16 +0200

    Правки по ревью: ушел от использования typing

--- src/movies/models/film.py
+++ src/movies/models/film.py
@@ -1,14 +1,38 @@
 from __future__ import annotations
 
-from typing import Optional, List
+import uuid
 from pydantic import BaseModel
 
 
-class Film(BaseModel):
-    id: str
+class PersonMixin:
+    id: uuid.UUID
+    name: str
+
+
+class Director(BaseModel, PersonMixin):
+    pass
+
+
+class Actor(BaseModel, PersonMixin):
+    pass
+
+
+class Writer(BaseModel, PersonMixin):
+    pass
+
+
+class Genre(BaseModel):
+    id: uuid.UUID
     title: str
-    description: Optional[str]
+
+
+class Film(BaseModel):
+    id: uuid.UUID
+    title: str
+    description: str | None
     imdb_rating: float
-    directors: Optional[List]
-    actors: Optional[List]
-    writers: Optional[List]
+    # @todo TMP
+    # genres: list[Genre]
+    directors: list[Director] | None
+    actors: list[Actor] | None
+    writers: list[Writer] | None
