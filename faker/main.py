5e6bd856556bd58a07b483238db11b848829c7f9
ishar8520 <isharov8520@gmail.com>
2024-12-11 16:34:25 +0300

    Исправил ошибки

--- faker/main.py
+++ faker/main.py
@@ -19,8 +19,9 @@ DB_CONFIG = {
     'port': db_config.port,
 }
 
-NUMBER_OF_FILMS = 1
-NUMBER_OF_PERSONS = 15
+NUMBER_OF_FILMS = 200000
+NUMBER_OF_PERSONS = 4000
+TIME_NOW = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")
 
 GENRES = {
     'Action': 'Action movies',
@@ -57,80 +58,72 @@ ROLES = [
     'director',
 ]
 
+
 def generate_films_data():
-    time_now = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")
-    data = {
-        'id': str(uuid4()),
-        'title': fake.sentence(nb_words=3),
-        'description': fake.text(max_nb_chars=200),
-        'imdb_rating': str(round(random.uniform(1.0, 10.0), 1)),
-        'type': 'movie',
-        'created': time_now,
-        'modified': time_now,
-    }
-    return list(data.values())
+    id = str(uuid4())
+    title = fake.sentence(nb_words=3)
+    description = fake.text(max_nb_chars=200).replace('\n', '')
+    rating = str(round(random.uniform(1.0,10.0), 1))
+    type = 'movie'
+    created = TIME_NOW
+    modified = TIME_NOW
+    return [id, title, description, rating, type, created, modified]
 
 
 def generate_person_data():
-    time_now = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")
-    data = {
-        'id': str(uuid4()),
-        'full_name': fake.name(),
-        'created': time_now,
-        'modified': time_now,
-    }
-    return list(data.values())
+    id = str(uuid4())
+    full_name = fake.name()
+    created = TIME_NOW
+    modified = TIME_NOW
+    return [id, full_name, created, modified]
 
-def generate_genre_data(genre, discription):
-    time_now = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")
-    data = {
-        'id': str(uuid4()),
-        'genre': genre,
-        'description': discription,
-        'created': time_now,
-        'modified': time_now,
-    }
-    return list(data.values())
 
-def generate_genre_film_data(genre_id, film_id):
-    time_now = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")
-    data = {
-        'id': str(uuid4()),
-        'genre_id': genre_id,
-        'film_id': film_id,
-        'created': time_now
-    }
-    return list(data.values())
+def generate_genre_data(genre, description):
+    id = str(uuid4())
+    created = TIME_NOW
+    modified = TIME_NOW
+    return [id, genre, description, created, modified]
 
-def generate_person_film_data(person_id, film_id, role):
-    time_now = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")
-    data = {
-        'id': str(uuid4()),
-        'person_id': person_id,
-        'film_id': film_id,
-        'role': role,
-        'created': time_now,
-    }
-    return list(data.values())
+
+def generate_genre_film_relation(genre_id, film_id):
+    id = str(uuid4())
+    created = TIME_NOW
+    return [id, genre_id, film_id, created]
+
+
+def generate_person_film_relation(person_id, film_id, role):
+    id = str(uuid4())
+    created = TIME_NOW
+    return [id, person_id, film_id, role, created]
+
+
+def copy_query(conn: psycopg2.connect, cursor: DictCursor, table: str, fields: str, copy_data: str):
+    query = f"""
+            COPY content.{table} {fields} FROM stdin;
+            """
+    cursor.copy_expert(query, StringIO(copy_data))
+    return conn.commit()
+
 
 if __name__ == '__main__':
     with psycopg2.connect(**DB_CONFIG) as conn:
         with conn.cursor(cursor_factory=DictCursor) as cursor:
+            
             genres_list = [generate_genre_data(genre, disription) for genre, disription in GENRES.items()]
-            genres_query = '\n'.join(['\t'.join(genre) for genre in genres_list])
-            query = """
-                    COPY content.genre (id, name, description, created, modified) FROM stdin; 
-                    """
-            cursor.copy_expert(query, StringIO(genres_query))
+            genres_query = '\n'.join(['\t'.join(genre) for genre in genres_list])     
+            copy_query(conn=conn,
+                       cursor=cursor,
+                       table='genre',
+                       fields='(id, name, description, created, modified)',
+                       copy_data=genres_query)
             
             persons_list = [generate_person_data() for x in range(NUMBER_OF_PERSONS)]
             persons_query = '\n'.join(['\t'.join(person) for person in persons_list])
-            
-            query = """
-                    COPY content.person (id, full_name, created, modified) FROM stdin; 
-                    """
-            cursor.copy_expert(query, StringIO(persons_query))
-            conn.commit()
+            copy_query(conn=conn,
+                       cursor=cursor,
+                       table='person',
+                       fields='(id, full_name, created, modified)',
+                       copy_data=persons_query)
             
             films_list = []
             genres_films_list = []
@@ -143,30 +136,30 @@ if __name__ == '__main__':
                 film = generate_films_data()
                 films_list.append(film)
                 for genre in genre_film:
-                    genres_films_list.append(generate_genre_film_data(genre[0], film[0]))
+                    genres_films_list.append(generate_genre_film_relation(genre[0], film[0]))
                 for actor in actors:
-                    persons_films_list.append(generate_person_film_data(actor[0], film[0], 'actor'))
+                    persons_films_list.append(generate_person_film_relation(actor[0], film[0], 'actor'))
                 for director in directors:
-                    persons_films_list.append(generate_person_film_data(director[0], film[0], 'director'))
+                    persons_films_list.append(generate_person_film_relation(director[0], film[0], 'director'))
                 for writer in writers:
-                    persons_films_list.append(generate_person_film_data(writer[0], film[0], 'writer'))
+                    persons_films_list.append(generate_person_film_relation(writer[0], film[0], 'writer'))
                     
             films_string = '\n'.join(['\t'.join(film) for film in films_list])
             genres_films_string = '\n'.join(['\t'.join(genre) for genre in genres_films_list])
             persons_films_string = '\n'.join(['\t'.join(person) for person in persons_films_list])
             
-            query_film = """
-                COPY content.film_work (id, title, description, rating, type, created, modified) FROM stdin;
-            """
-            query_genre_film = """
-                COPY content.genre_film_work (id, genre_id, film_work_id, created) FROM stdin;
-            """
-            query_person_film = """
-                COPY content.person_film_work (id, person_id, film_work_id, role, created) FROM stdin;
-            """
-            
-            cursor.copy_expert(query_film, StringIO(films_string))
-            cursor.copy_expert(query_genre_film, StringIO(genres_films_string))
-            cursor.copy_expert(query_person_film, StringIO(persons_films_string))
-            
-            conn.commit()
+            copy_query(conn=conn,
+                       cursor=cursor,
+                       table='film_work',
+                       fields='(id, title, description, rating, type, created, modified)',
+                       copy_data=films_string)
+            copy_query(conn=conn,
+                       cursor=cursor,
+                       table='genre_film_work',
+                       fields='(id, genre_id, film_work_id, created)',
+                       copy_data=genres_films_string)
+            copy_query(conn=conn,
+                       cursor=cursor,
+                       table='person_film_work',
+                       fields='(id, person_id, film_work_id, role, created)',
+                       copy_data=persons_films_string)
