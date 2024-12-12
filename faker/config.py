0a4d31c92d32a5f214514a96e952a27499322ed5
ishar8520 <isharov8520@gmail.com>
2024-12-12 22:58:47 +0300

    Добавил сервис faker

--- faker/config.py
+++ faker/config.py
@@ -5,8 +5,8 @@ class DBConfig(BaseSettings):
     database: str
     username: str
     password: str
-    host: str = 'localhost'
-    port: int = 6432
+    host: str = 'faker-postgresql'
+    port: int = 5432
 
     class Config:
         env_prefix = 'POSTGRESQL_'
