b4c5b27779f88fd5ddc60345770a9efa6cbe7900
Artyom Opanchuk <subzero666@list.ru>
2025-01-15 00:52:23 +0300

    Таблицы БД для пользователей и ролей

---
+++ compose/auth/auth/models/sqlalchemy.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+import datetime
+import uuid
+
+from sqlalchemy import (
+    UUID,
+    TEXT,
+    Boolean,
+    DateTime,
+    ForeignKey,
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+)
+
+from ..db.sqlalchemy import AuthBase
+
+
+class User(AuthBase):
+    __tablename__ = 'user'
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID,
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    email: Mapped[str] = mapped_column(TEXT, unique=True)
+    hashed_password: Mapped[str] = mapped_column(TEXT)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
+    created: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.datetime.now(datetime.UTC),
+    )
+    modified: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.datetime.now(datetime.UTC),
+        onupdate=lambda: datetime.datetime.now(datetime.UTC),
+    )
+
+
+class Role(AuthBase):
+    __tablename__ = 'role'
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID,
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    name: Mapped[str] = mapped_column(TEXT)
+    code: Mapped[str] = mapped_column(TEXT)
+    created: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.datetime.now(datetime.UTC),
+    )
+    modified: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.datetime.now(datetime.UTC),
+        onupdate=lambda: datetime.datetime.now(datetime.UTC),
+    )
+
+
+class UserRole(AuthBase):
+    __tablename__ = 'user_role'
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID,
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('auth.user.id'))
+    role_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('auth.role.id'))
+    created: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.datetime.now(datetime.UTC),
+    )
