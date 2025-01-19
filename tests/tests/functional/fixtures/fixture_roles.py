ca9d78ff939b731fb6c277a3a9b99e53703933d5
ishar8520 <isharov8520@gmail.com>
2025-01-19 15:29:26 +0300

    Update fixture_roles.py

--- tests/tests/functional/fixtures/fixture_roles.py
+++ tests/tests/functional/fixtures/fixture_roles.py
@@ -8,7 +8,7 @@ from ..utils.auth.sqlalchemy import Role
 
 @pytest_asyncio.fixture(scope='module')
 async def create_role(async_session: AsyncSession):
-    new_role = Role(name="subscribers", code="subscribers")
+    new_role = Role(name='subscribers', code='subscribers')
     async_session.add(new_role)
     await async_session.commit()
     await async_session.refresh(new_role)
