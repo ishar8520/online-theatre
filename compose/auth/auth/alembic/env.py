ee5ec09c8c93234c07b202ff85c66a4ef1cb6e03
Artyom Opanchuk <subzero666@list.ru>
2025-01-23 14:10:51 +0300

    Добавил контейнер для запуска миграций

--- compose/auth/auth/alembic/env.py
+++ compose/auth/auth/alembic/env.py
@@ -8,8 +8,8 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from ..core import settings
-from ..db.sqlalchemy import auth_metadata_obj
+from auth.core import settings
+from auth.db.sqlalchemy import auth_metadata_obj
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = auth_metadata_obj
+target_metadata = [auth_metadata_obj]
 
 
 # other values from the config, defined by the needs of env.py,
