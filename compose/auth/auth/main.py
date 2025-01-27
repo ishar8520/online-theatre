fb9550aa694d66abe79567ec8acb0b8ad8f1f85f
Artyom Opanchuk <subzero666@list.ru>
2025-01-27 18:54:35 +0300

    Подключил Jaeger

--- compose/auth/auth/main.py
+++ compose/auth/auth/main.py
@@ -1,11 +1,22 @@
 from __future__ import annotations
 
 import logging.config
-from collections.abc import AsyncGenerator
+from collections.abc import AsyncGenerator, Callable, Awaitable
 from contextlib import asynccontextmanager
 
 import redis.asyncio as redis
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response, HTTPException, status
+from opentelemetry import trace
+from opentelemetry.baggage.propagation import W3CBaggagePropagator
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.propagate import set_global_textmap
+from opentelemetry.propagators.composite import CompositePropagator
+from opentelemetry.propagators.jaeger import JaegerPropagator
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 
 from .api.v1.endpoints import (
     roles,
@@ -21,9 +32,32 @@ from .core import settings, LOGGING
 logging.config.dictConfig(LOGGING)
 
 
+def configure_otel() -> None:
+    if not settings.otel.enabled:
+        return
+
+    resource = Resource(attributes={
+        SERVICE_NAME: settings.otel.service_name,
+    })
+    tracer_provider = TracerProvider(resource=resource)
+    trace.set_tracer_provider(tracer_provider)
+
+    span_exporter = OTLPSpanExporter(endpoint=settings.otel.exporter_otlp_http_endpoint)
+    span_processor = BatchSpanProcessor(span_exporter)
+    tracer_provider.add_span_processor(span_processor)
+
+    set_global_textmap(CompositePropagator([
+        TraceContextTextMapPropagator(),
+        W3CBaggagePropagator(),
+        JaegerPropagator(),
+    ]))
+
+
 # noinspection PyUnusedLocal,PyShadowingNames
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[dict]:
+    configure_otel()
+
     async with (
         redis.Redis(host=settings.redis.host, port=settings.redis.port) as redis_client,
     ):
@@ -40,6 +74,22 @@ app = FastAPI(
     openapi_url=f'{base_api_prefix}/openapi.json',
     lifespan=lifespan,
 )
+FastAPIInstrumentor.instrument_app(app, http_capture_headers_server_request=['X-Request-Id'])
+
+
+@app.middleware('http')
+async def check_request_id(request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
+    if settings.otel.enabled and settings.otel.request_id_required:
+        request_id = request.headers.get('X-Request-Id')
+
+        if not request_id:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='X-Request-Id is required',
+            )
+
+    return await call_next(request)
+
 
 auth_api_prefix = f'{base_api_prefix}/v1'
 
