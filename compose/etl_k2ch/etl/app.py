8430e20dedaff54e83a7eedd692233ebc4ec7cb1
Мария Пирогова <m.pirogova@bars-open.ru>
2025-02-23 22:36:36 +0300

    Добавлена вставка батчами

--- compose/etl_k2ch/etl/app.py
+++ compose/etl_k2ch/etl/app.py
@@ -1,38 +1,40 @@
+import logging
 from time import sleep
 
-from src.services.exceptions import (
-    InvalidTransformData,
-    UnknownTransformerType
-)
-from src.services.kafka_consumer import KafkaConsumerService
+from src.core.config import settings
 from src.services.clickhouse import ClickhouseService
 from src.services.data_transformer import TransformerFactory
-import logging
+from src.services.exceptions import (
+    InvalidTransformData,
+    UnknownTransformerType,
+)
+from src.services.kafka_consumer import KafkaConsumerService
 
 
 def main() -> None:
     kafka_service = KafkaConsumerService()
-    clickhouse_client = ClickhouseService()
+    clickhouse_client = ClickhouseService(batch_size=settings.batch_size, flush_interval=settings.flush_interval)
 
     while True:
         message_pack = kafka_service.poll()
         if not message_pack:
             sleep(2)
 
+        clickhouse_client.check_auto_flush()
+
+        if not message_pack:
+            continue
+
         for topic, messages in message_pack.items():
             for message in messages:
                 data_json = message.value.decode("utf-8")
-
                 try:
                     transformer = TransformerFactory.get(
                         event_name=topic[0],
                         data=data_json
                     )
-                    data = transformer.transform()
-
-                    clickhouse_client.insert(transformer.get_type(), data)
-                    kafka_service.commit()
-
+                    transformed_data = transformer.transform()
+                    clickhouse_client.add_to_batch(transformer.get_type(), transformed_data)
                     logging.info('Success insert!')
                 except InvalidTransformData:
                     logging.error('Invalid transform data!')
