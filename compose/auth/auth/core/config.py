9aacb9ce316e2dea8e79c4086b702045f45a2d3e
Мария Пирогова <m.pirogova@bars-open.ru>
2025-03-06 14:17:13 +0300

    Добавлена интеграция с Sentry io

--- compose/auth/auth/core/config.py
+++ compose/auth/auth/core/config.py
@@ -70,6 +70,16 @@ class RateLimiterConfig(BaseSettings):
     seconds: int = 60
 
 
+class SentryConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix='sentry_')
+
+    dsn: str = ''
+    enable_sdk: bool = False
+    enable_tracing: bool = False
+    traces_sample_rate: float = 1.0
+    profiles_sample_rate: float = 1.0
+
+
 # noinspection PyArgumentList
 class Settings(BaseSettings):
     auth: AuthConfig = AuthConfig()
@@ -79,9 +89,11 @@ class Settings(BaseSettings):
     cache: CacheConfig = CacheConfig()
     ratelimiter: RateLimiterConfig = RateLimiterConfig()
     otel: OpenTelemetryConfig = OpenTelemetryConfig()
+    sentry: SentryConfig = SentryConfig()
 
     @property
     def test_mode(self) -> bool:
         return self.auth.test_mode
+
 
 settings = Settings()
