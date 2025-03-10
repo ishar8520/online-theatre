fcf88d1daf329e9569f0bb7171871f66eacac7da
Maxim Lashkov <maxim.lashkov@yandex.ru>
2025-03-10 20:37:05 +0200

    Правки по результатам mypy

--- tests/tests/functional/fixtures/fixture_mongo.py
+++ tests/tests/functional/fixtures/fixture_mongo.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from collections.abc import AsyncGenerator
+from collections.abc import Callable, Awaitable, AsyncGenerator
 
 import pytest_asyncio
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
@@ -9,15 +9,18 @@ from ..settings import settings
 
 
 @pytest_asyncio.fixture(scope='session')
-async def mongo_client_db() -> AsyncGenerator[AsyncIOMotorDatabase]:
-    client = AsyncIOMotorClient(f'mongodb://{settings.mongo.host}:{settings.mongo.port}')
+async def mongo_client_db() -> AsyncGenerator[AsyncIOMotorDatabase, None]:
+    client: AsyncIOMotorClient = AsyncIOMotorClient(f'mongodb://{settings.mongo.host}:{settings.mongo.port}')
     db = client.get_database(settings.mongo.db)
     yield db
     client.close()
 
 
 @pytest_asyncio.fixture(scope='function', autouse=True)
-async def clear_mongo(mongo_client_db: AsyncIOMotorDatabase) -> None:
+async def clear_mongo(
+        mongo_client_db: AsyncIOMotorDatabase
+) -> AsyncGenerator[Callable[[], Awaitable[None]]]:
+
     async def _clear_mongo() -> None:
         collection_name_list = await mongo_client_db.list_collection_names()
         for name in collection_name_list:
