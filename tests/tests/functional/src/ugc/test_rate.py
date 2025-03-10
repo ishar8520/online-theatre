eb7b8d8c83d2261d989c4ab885839fd87b8c96cd
Maxim Lashkov <maxim.lashkov@yandex.ru>
2025-03-10 18:24:46 +0200

    Тест для проверки метода rate/delete

--- tests/tests/functional/src/ugc/test_rate.py
+++ tests/tests/functional/src/ugc/test_rate.py
@@ -66,3 +66,65 @@ async def test_add_duplicate(
     async with aiohttp_session.put(url, json=input_data) as response:
         status = response.status
         assert status == expected["status"]
+
+
+@pytest.mark.parametrize(
+    "input_data, expected",
+    [
+        (
+            {
+                "user_id": "792c57e4-d8b8-40b7-8e5b-260f6db62a68",
+                "film_id": "792c57e4-d8b8-40b7-8e5b-260f6db62333",
+                "rate": 8
+            },
+            {
+                "status": HTTPStatus.OK
+            }
+        ),
+    ]
+)
+@pytest.mark.asyncio(loop_scope='session')
+async def test_delete(
+        aiohttp_session,
+        clear_mongo,
+        input_data,
+        expected
+) -> None:
+    url = urljoin(settings.ugc_api_v1_url, 'rate/add')
+    async with aiohttp_session.put(url, json=input_data) as response_add:
+        assert response_add.status == HTTPStatus.OK
+        result_add = await response_add.json()
+
+        assert "id" in result_add
+
+        url = urljoin(settings.ugc_api_v1_url, f'rate/delete/{result_add["id"]}')
+        async with aiohttp_session.delete(url, json=input_data) as response_del:
+            assert response_del.status == expected["status"]
+
+            result_del = await response_del.json()
+            assert result_del["result"] == True
+
+
+@pytest.mark.parametrize(
+    "input_data, expected",
+    [
+        (
+            {
+                "id": "67cdb68360862805fc1f6506"
+            },
+            {
+                "status": HTTPStatus.NOT_FOUND
+            }
+        ),
+    ]
+)
+@pytest.mark.asyncio(loop_scope='session')
+async def test_delete_by_unknown_id(
+        aiohttp_session,
+        clear_mongo,
+        input_data,
+        expected
+) -> None:
+    url = urljoin(settings.ugc_api_v1_url, f'rate/delete/{input_data["id"]}')
+    async with aiohttp_session.delete(url, json=input_data) as response_del:
+        assert response_del.status == expected["status"]
