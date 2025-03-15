5835e2735476275e8068973883a8a684268453c8
Artyom Opanchuk <subzero666@list.ru>
2025-03-15 22:28:20 +0300

    Метод API для получения информации о пользователе

--- compose/auth/auth/api/v1/endpoints/users/users.py
+++ compose/auth/auth/api/v1/endpoints/users/users.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
+import uuid
+
 from fastapi import APIRouter, HTTPException, status
 
 from .common import ErrorCode, ErrorModel
 from .....services.users import (
     CurrentUserDep,
+    CurrentSuperuserDep,
     UserManagerDep,
     UserRead,
     UserUpdate,
+    UserDoesNotExist,
     UserAlreadyExists,
 )
 from .....services.users.authentication.login_history.dependencies import PageDep
@@ -18,36 +22,36 @@ router = APIRouter()
 
 
 @router.get(
-    "/me",
+    '/me',
+    name='users:get_current_user',
     response_model=UserRead,
-    name='users:current_user',
     responses={
         status.HTTP_401_UNAUTHORIZED: {
-            "description": "Token is invalid or missing.",
+            'description': 'Token is invalid or missing.',
         },
     },
 )
-async def me(user: CurrentUserDep):
+async def get_current_user(user: CurrentUserDep) -> UserRead:
     return UserRead.model_validate(user, from_attributes=True)
 
 
 @router.patch(
-    "/me",
-    response_model=UserRead,
+    '/me',
     name='users:patch_current_user',
+    response_model=UserRead,
     responses={
         status.HTTP_401_UNAUTHORIZED: {
-            "description": "Token is invalid or missing.",
+            'description': 'Token is invalid or missing.',
         },
         status.HTTP_400_BAD_REQUEST: {
-            "model": ErrorModel,
-            "content": {
-                "application/json": {
-                    "examples": {
+            'model': ErrorModel,
+            'content': {
+                'application/json': {
+                    'examples': {
                         ErrorCode.UPDATE_USER_LOGIN_ALREADY_EXISTS: {
-                            "summary": "A user with this login already exists.",
-                            "value": {
-                                "detail": ErrorCode.UPDATE_USER_LOGIN_ALREADY_EXISTS
+                            'summary': 'A user with this login already exists.',
+                            'value': {
+                                'detail': ErrorCode.UPDATE_USER_LOGIN_ALREADY_EXISTS
                             },
                         },
                     }
@@ -56,19 +60,48 @@ async def me(user: CurrentUserDep):
         },
     },
 )
-async def update_me(
-        user: CurrentUserDep,
-        user_update: UserUpdate,
-        user_manager: UserManagerDep,
-):
+async def patch_current_user(user: CurrentUserDep,
+                             user_update: UserUpdate,
+                             user_manager: UserManagerDep) -> UserRead:
     try:
         user = await user_manager.update(user_update, user)
-        return UserRead.model_validate(user, from_attributes=True)
     except UserAlreadyExists:
         raise HTTPException(
             status.HTTP_400_BAD_REQUEST,
             detail=ErrorCode.UPDATE_USER_LOGIN_ALREADY_EXISTS,
         )
+
+    return UserRead.model_validate(user, from_attributes=True)
+
+
+@router.get(
+    '/{user_id}',
+    name='users:get_user',
+    response_model=UserRead,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            'description': 'Missing token or inactive user.',
+        },
+        status.HTTP_403_FORBIDDEN: {
+            'description': 'Not a superuser.',
+        },
+        status.HTTP_404_NOT_FOUND: {
+            'description': 'The user does not exist.',
+        },
+    },
+)
+async def get_user(user_id: uuid.UUID,
+                   user_manager: UserManagerDep,
+                   _current_superuser: CurrentSuperuserDep) -> UserRead:
+    try:
+        user = await user_manager.get(user_id)
+    except UserDoesNotExist:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=ErrorCode.USER_DOES_NOT_EXIST,
+        )
+
+    return UserRead.model_validate(user, from_attributes=True)
 
 
 @router.get(
