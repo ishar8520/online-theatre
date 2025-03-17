954650e7c1473aa4539c8b2b2ad52fa80f4004df
Мария Пирогова <m.pirogova@bars-open.ru>
2025-03-17 16:26:20 +0300

    Заготовка административной панели для создания рассылок менеджерами проекта

---
+++ compose/notification_api/schemas/admin_notification.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class CreateAdminNotificationSchema(BaseModel):
+    notification_type: str
+    delivery_type: str
+    send_date: datetime | None = None
+
+
+class GetAdminNotificationSchema(BaseModel):
+    id: UUID
+    notification_type: str
+    delivery_type: str
+    status: str
+    created: datetime
+    send_date: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UpdateNotificationSchema(BaseModel):
+    send_date: datetime
