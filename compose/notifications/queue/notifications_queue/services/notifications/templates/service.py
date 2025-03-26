c47b59978a55c992e501a2b84d7814f4d12dc502
Maxim Lashkov <maxim.lashkov@yandex.ru>
2025-03-26 23:06:30 +0200

    Добавил рендер шаблона при помощи jinja2

--- compose/notifications/queue/notifications_queue/services/notifications/templates/service.py
+++ compose/notifications/queue/notifications_queue/services/notifications/templates/service.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import abc
 import logging
+
+import jinja2
 from typing import Annotated
 
 from taskiq import TaskiqDepends
@@ -47,16 +49,37 @@ class NotificationTemplateService(AbstractNotificationTemplateService):
         logger.info('NotificationTemplateService.render_template_notification()')
         logger.info('notification=%r', notification)
 
+        template_string = await self._get_template(notification)
+        message_text = await self._render_text(
+            template_string,
+            notification.template_context
+        )
+
         message = NotificationMessage(
             type=notification.type,
             subject=notification.subject,
-            text='<Rendered message text>',
+            text=message_text,
         )
+
+        logger.info('notification=%r', message)
 
         await process_notification_users_task.kiq(  # type: ignore[call-overload]
             message=message,
             users=notification.users
         )
+
+    @staticmethod
+    async def _get_template(notification: TemplateNotification) -> str:
+        if notification.template_id is not None:
+            # todo get template by id
+            return ''
+
+        # todo get template by code
+        return ''
+
+    @staticmethod
+    async def _render_text(template_string: str, data: dict) -> str:
+        return jinja2.Template(source=template_string).render(data)
 
 
 async def get_notification_template_service() -> AbstractNotificationTemplateService:
