efb6e2d822e6831eccd72881c925d93b1fd9ca4d
ishar8520 <ishar8520@gmail.com>
2025-03-27 23:10:37 +0300

    фикс

--- compose/notifications/queue/notifications_queue/services/rabbitmq.py
+++ compose/notifications/queue/notifications_queue/services/rabbitmq.py
@@ -1,18 +1,20 @@
-from aio_pika import connect, Connection, Channel
+from typing import Optional
+from aio_pika import connect
+from aio_pika.abc import AbstractConnection, AbstractChannel
 from notifications_queue.core.config import settings
 
 class RabbitMQ:
-    _connection: Connection = None
-    _channel: Channel = None
+    _connection: Optional[AbstractConnection] = None
+    _channel: Optional[AbstractChannel] = None
 
     @classmethod
-    async def get_connection(cls) -> Connection:
+    async def get_connection(cls) -> AbstractConnection:
         if cls._connection is None or cls._connection.is_closed:
             cls._connection = await connect(settings.rabbitmq.url)
         return cls._connection
 
     @classmethod
-    async def get_channel(cls) -> Channel:
+    async def get_channel(cls) -> AbstractChannel:
         if cls._channel is None or cls._channel.is_closed:
             connection = await cls.get_connection()
             cls._channel = await connection.channel()
@@ -22,5 +24,7 @@ class RabbitMQ:
     async def close(cls):
         if cls._channel and not cls._channel.is_closed:
             await cls._channel.close()
+            cls._channel = None
         if cls._connection and not cls._connection.is_closed:
             await cls._connection.close()
+            cls._connection = None
