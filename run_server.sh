9be4b67bf13421b9a65b4428963dc3356537f91a
Maxim Lashkov <maxim.lashkov@yandex.ru>
2025-04-09 00:13:04 +0200

    Добавил отдельный конфиг для nginx для легкой разработки сервиса. Плюс отдельный запуск через run_server с ключом billing

--- run_server.sh
+++ run_server.sh
@@ -10,12 +10,17 @@ run_server_dev() {
     docker compose -f compose.dev.yaml up --watch "$@"
 }
 
+run_server_billing() {
+    docker compose -f compose.billing.yaml up --watch "$@"
+}
+
 main() {
     local command="$1"
     local docker_compose_args=(--pull always --build --force-recreate --remove-orphans)
 
     case "$command" in
         dev) run_server_dev "${docker_compose_args[@]}"; exit ;;
+        billing) run_server_billing "${docker_compose_args[@]}"; exit ;;
     esac
 
     run_server "${docker_compose_args[@]}"
